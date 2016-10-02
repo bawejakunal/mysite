1a109307672fdeb2c7b55c7c11c50f95cdfe22ba
Kunal Baweja <bawejakunal15@gmail.com>
2016-10-02 18:06:46 -0400

    index view test no question

--- polls/tests.py
+++ polls/tests.py
@@ -21,12 +21,6 @@ class QuestionMethodTests(TestCase):
         self.assertIs(future_question.was_published_recently(),False)
 
 
-def create_question(question_text, days):
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
-
-
-class QuestionViewTests(TestCase):
     def test_was_published_recently_with_old_question(self):
         """
             was_published_recently should return False for questions whoe pub_date is older than 1 day.
@@ -42,3 +36,19 @@ class QuestionViewTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+            If no questions exits, an appropriate error message
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
