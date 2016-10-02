4421344a0ee9f50f5a600a7631c1a619469f42a7
Kunal Baweja <bawejakunal15@gmail.com>
2016-10-02 14:06:31 -0400

    add more views

--- polls/views.py
+++ polls/views.py
@@ -9,3 +9,13 @@ def index(request):
         index view: welcome page
     """
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
