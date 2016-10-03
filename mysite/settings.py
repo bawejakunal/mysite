c8e3d0e0df79b52c7c68864b255cd115767ed2dc
Kunal Baweja <bawejakunal15@gmail.com>
2016-10-02 21:20:28 -0400

    do not ignore migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pollsdb',
-        'USER': 'travis',
+        'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': '',
     }
 }
 
